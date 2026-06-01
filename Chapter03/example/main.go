23b4ae8349c4c7eb3cf05c3e8c69d61cd47d40b1
Andy Williams <andy@andy.xyz>
2026-06-01 12:17:42 +0100

    Importing Chapters 2, 3 and 4 from first edition

---
+++ Chapter03/example/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"image/color"
+	"time"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+)
+
+type snakePart struct {
+	x, y float32
+}
+
+type moveType int
+
+const (
+	moveUp moveType = iota
+	moveDown
+	moveLeft
+	moveRight
+)
+
+var (
+	snakeParts []snakePart
+	game       *fyne.Container
+	move       = moveUp
+)
+
+func keyTyped(e *fyne.KeyEvent) {
+	switch e.Name {
+	case fyne.KeyUp:
+		move = moveUp
+	case fyne.KeyDown:
+		move = moveDown
+	case fyne.KeyLeft:
+		move = moveLeft
+	case fyne.KeyRight:
+		move = moveRight
+	}
+}
+
+func main() {
+	a := app.New()
+
+	w := a.NewWindow("Snake")
+	w.Resize(fyne.NewSize(200, 200))
+	w.SetFixedSize(true)
+
+	game = setupGame()
+	w.SetContent(game)
+	w.Canvas().SetOnTypedKey(keyTyped)
+
+	go runGame()
+	w.ShowAndRun()
+}
+
+func refreshGame() {
+	for i, seg := range snakeParts {
+		rect := game.Objects[i]
+		rect.Move(fyne.NewPos(seg.x*10, seg.y*10))
+	}
+
+	game.Refresh()
+}
+
+func runGame() {
+	for {
+		time.Sleep(time.Millisecond * 250)
+		for i := len(snakeParts) - 1; i >= 1; i-- {
+			snakeParts[i] = snakeParts[i-1]
+		}
+
+		switch move {
+		case moveUp:
+			snakeParts[0].y--
+		case moveDown:
+			snakeParts[0].y++
+		case moveLeft:
+			snakeParts[0].x--
+		case moveRight:
+			snakeParts[0].x++
+		}
+		fyne.Do(refreshGame)
+	}
+}
+
+func setupGame() *fyne.Container {
+	var segments []fyne.CanvasObject
+	for i := 0; i < 10; i++ {
+		seg := snakePart{9, float32(5 + i)}
+		snakeParts = append(snakeParts, seg)
+
+		r := canvas.NewRectangle(&color.RGBA{G: 0x66, A: 0xff})
+		r.Resize(fyne.NewSize(10, 10))
+		r.Move(fyne.NewPos(90, float32(50+i*10)))
+		segments = append(segments, r)
+	}
+
+	return container.NewWithoutLayout(segments...)
+}
---
+++ Chapter04/example/image.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"log"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/storage"
+	"github.com/nfnt/resize"
+)
+
+func scaleImage(img image.Image) image.Image {
+	return resize.Thumbnail(320, 240, img, resize.Lanczos3)
+}
+
+func loadImage(u fyne.URI) fyne.CanvasObject {
+	res, err := storage.LoadResourceFromURI(u)
+	if err != nil {
+		log.Println("Error reading image", err)
+		return canvas.NewRectangle(color.Black)
+	}
+	img := canvas.NewImageFromResource(res)
+	img.FillMode = canvas.ImageFillContain
+	return img
+}
+
+type itemLayout struct {
+	bg, text, gradient fyne.CanvasObject
+}
+
+func (i *itemLayout) MinSize(_ []fyne.CanvasObject) fyne.Size {
+	return fyne.NewSize(160, 120)
+}
+
+func (i *itemLayout) Layout(objs []fyne.CanvasObject, size fyne.Size) {
+	textHeight := float32(22)
+	for _, o := range objs {
+		if o == i.text {
+			o.Move(fyne.NewPos(0, size.Height-textHeight))
+			o.Resize(fyne.NewSize(size.Width, textHeight))
+		} else if o == i.bg {
+			o.Move(fyne.NewPos(0, size.Height-textHeight))
+			o.Resize(fyne.NewSize(size.Width, textHeight))
+		} else if o == i.gradient {
+			o.Move(fyne.NewPos(0, size.Height-(textHeight*1.5)))
+			o.Resize(fyne.NewSize(size.Width, textHeight/2))
+		} else {
+			o.Move(fyne.NewPos(0, 0))
+			o.Resize(size)
+		}
+	}
+}
+
+func makeImageItem(u fyne.URI) fyne.CanvasObject {
+	label := canvas.NewText(u.Name(), color.Gray{128})
+	label.Alignment = fyne.TextAlignCenter
+
+	bgColor := &color.NRGBA{R: 255, G: 255, B: 255, A: 224}
+	bg := canvas.NewRectangle(bgColor)
+	fade := canvas.NewLinearGradient(color.Transparent, bgColor, 0)
+	return container.New(&itemLayout{text: label, bg: bg, gradient: fade},
+		loadImage(u), bg, fade, label)
+}
